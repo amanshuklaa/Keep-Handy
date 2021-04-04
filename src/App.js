f9cc2178d9f64791e72f9a5b81a4b6c77be266ba
aman <amanshukla@amans-MacBook-Pro.local>
2021-04-04 12:06:29 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React , {useState} from 'react'
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import CreateNote  from './CreateNote'
+import Note from './Note'
+
+function App() {
+const [addItem, setAddItem] = useState([]);
+  const addNote = (note) =>{
+   setAddItem((prevData) =>{
+     return [...prevData,note];
+   });
+   
+
+  }
+  const onDelete = (id) =>{
+    setAddItem((oldData) =>
+      oldData.filter((currdata,index) =>{
+          return index !== id;
+      })
+    )
+  }
+
+  return (
+    <div className="App">
+      <Header />
+     
+      <CreateNote passNote= {addNote} />
+     { addItem.map((val,index)=>{
+        return <Note
+          key = {index}
+          id = {index}
+          title = {val.title}
+          content = {val.content}
+          deleteItem = {onDelete}
+        />
+      })}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CreateNote.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const CreateNote = (props) => {
+const [expand, setExpand] = useState(false)
+const [note, setNote] = useState({
+    title:"",
+    content:"",
+});
+const inputEvent = (event) =>{
+    const {name, value} = event.target;
+
+    setNote((prevData) =>{
+        return {
+            ...prevData,
+            [name]: value
+        }
+    })
+    console.log(note)
+}
+
+const addEvent = () =>{
+    props.passNote(note);
+    setNote({
+        title:"",
+        content:"",
+    })
+}
+const expandIt = () =>{
+    setExpand(true)
+}
+const btoNormal = () =>{
+    setExpand(false)
+}
+    return (
+       <>
+        <div className ="main_note"  onDoubleClick ={btoNormal}>
+        <form>
+       {     expand ?
+            <input type ="text" 
+            name = "title"
+            value ={note.title} 
+            onChange ={inputEvent} 
+            placeholder="Title" 
+            autoComplete = "off" />:null}
+
+            
+            <textarea rows=""
+            name="content"
+            onClick = {expandIt}
+             column = ""
+              value ={note.content}
+               onChange ={inputEvent}
+
+                placeholder="Write a note..."/>
+          {  expand ?
+            <Button onClick = {addEvent}>
+            <AddIcon className ="plus_sign" />
+            </Button> :null}
+
+        </form>
+
+        </div>
+       </>
+    )
+}
+
+export default CreateNote
